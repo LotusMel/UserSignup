2396d1e32d24cc6b4582d92226e5b37cebbdfe74
Melva Lang <mjlang74@gmail.com>
2020-03-11 22:45:36 -0500

    Add project files.

---
+++ Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserSignup.Models
+{
+    public class User
+    {
+
+        public static string Username { get; set; }
+        public static string Email { get; set; }
+        public static string Password { get; set; }
+    }
+}
